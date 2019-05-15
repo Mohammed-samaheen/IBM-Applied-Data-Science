f9387564cb3581741f3329ae2acafdcdbf238d00
Gary N. Thomas <45762811+garynth41@users.noreply.github.com>
2019-05-15 08:30:21 -0400

    Introductory project scope notebook document.
    This is the initial layout of the project using Jupyter Notebook.

---
+++ IBM-Applied-Data-Science.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Capstone Project Notebook
+
+# The following Notebook will be used to get, clean, visualize and analyze data and information related to the completion of the IBM Capstone Project.
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[11]:
+
+
+import sys
+from notebook import transutils as _
+from notebook.services.contents.filemanager import FileContentsManager as FCM
+
+try:
+    notebook_fname = sys.argv[1].strip('.ipynb')
+except IndexError:
+    print("Usage: create-notebook <notebook>")
+    exit()
+
